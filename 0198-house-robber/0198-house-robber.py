5154f62767e73f4051f18b232d167be453b44778
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-07-27 15:48:17 +0530

    Time: 39 ms (94.11%), Space: 16.2 MB (95.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        return self.recursion(nums)
+        return self.iteration(nums)
 
-    def recursion(self, nums):
+    def recursion_with_memo(self, nums):
         memo = [-1 for _ in range(len(nums))]
         def recur(nums, i):
             if i < 0:
@@ -14,4 +14,14 @@ class Solution:
             return memo[i]
 
         return recur(nums, len(nums)-1)
+
+
+    def iteration(self, nums):
+        dp = [-1 for i in range(len(nums)+1)]
+        dp[0] = 0
+        dp[1] = nums[0]
+        for j in range(1,len(nums)):
+            dp[j+1] = max(dp[j], dp[j-1]+nums[j])
+        return dp[len(nums)]
+
 
