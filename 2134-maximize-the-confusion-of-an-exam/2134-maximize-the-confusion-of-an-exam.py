61e763b822f51b88b8719707e6e222f98631c958
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-08-16 21:23:55 -0400

    Time: 585 ms (13.83%), Space: 16.7 MB (48.66%) - LeetHub

---
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+
+        def maxConsecutiveXs(answer, k, param):
+            max_cnt = 0
+            L = 0
+            win_F_cnt = 0
+            for R in range(len(answer)):
+                if answer[R]==param:
+                    win_F_cnt+=1
+                while win_F_cnt > k:
+                    if answer[L]==param:
+                        win_F_cnt-=1
+                    L+=1
+                max_cnt = max(max_cnt, R-L+1)
+            return max_cnt
+        print(maxConsecutiveXs(answerKey, k, 'T'))
+        print(maxConsecutiveXs(answerKey, k, 'F'))
+        return max(maxConsecutiveXs(answerKey, k, 'T'), maxConsecutiveXs(answerKey, k, 'F'))
+
