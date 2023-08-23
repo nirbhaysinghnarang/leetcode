e88b4436946e576d4724ccc1f9d577c07c08f81c
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-08-23 12:13:19 -0400

    Time: 35 ms (91.15%), Space: 16.3 MB (94.07%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,34 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        g = defaultdict(list)
+        variables = set()
+        for i,(a,b) in enumerate(equations):
+            g[a].append((b, values[i]))
+            g[b].append((a, (1/values[i]))) 
+            variables.add(a)
+            variables.add(b)
+        def getValue(quot, div,visited, prod_so_far=1):
+            if quot not in variables or div not in variables:
+                return -1
+            if quot == div:
+                return prod_so_far
+            neighbours = g[quot]
+            visited.add(quot)
+            if(quot=="x2" and div=="x4"):
+                print(neighbours)
+            for n, runner in neighbours:
+                
+                if n not in visited:
+                    tmp = getValue(n, div, visited,prod_so_far*runner)
+                    if tmp!=-1:
+                        return tmp
+            return -1
+        arr = []
+        for q,div in queries:
+            visited = set()
+            arr.append(getValue(q,div, visited))
+        return arr
+
+            
+            
+
