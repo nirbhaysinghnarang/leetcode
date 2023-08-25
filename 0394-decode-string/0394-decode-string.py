09f8bd19dca51f89ad5909f78995fdd73b17e45c
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-08-25 12:10:48 -0400

    Time: 42 ms (50.72%), Space: 16.3 MB (38.51%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,26 +1,21 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        # 1. Keep adding to stack untill you get ]
-        # 2. When ] is found, dont add ] to stack, instead pop all characters until [ is found, pop [ too
-        # 3. Then keep popping till we have digits, keep adding to k
-        # 4. Multiply digit * string popped , and add this new string to stack
-
-        stack=[]
-        for i in range(len(s)):
-            if s[i] != ']':
-                stack.append(s[i]) # 1
+        stack = []
+        ret = ""
+        for char in s:
+            if char != ']':
+                stack.append(char)
             else:
-                substr=''
-                while stack and stack[-1]!= '[': # 2
-                    substr=stack.pop() + substr
+                toRep = ''
+                while stack and stack[-1]!='[':
+                   toRep = stack.pop() + toRep
                 
-                stack.pop() # 2. Popping [
-                k =''
-                while stack and stack[-1].isdigit(): # 3
-                    k = stack.pop() + k
-                
-                stack.append( int(k) * substr  ) # 4
-        
-        return ''.join(stack) # stack elements will be joined together directly without any characters in between
+                cnt = ''
+                stack.pop()
+                while stack and stack[-1].isnumeric():
+                    cnt = stack.pop() + cnt
 
+                stack.append(toRep*(int(cnt)))
+        return ''.join(stack)
+                
 
