93c547fc783a7d0b5e0cf0ef90cf2d2c0dcab485
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-08-26 19:05:07 -0400

    Time: 43 ms (63.24%), Space: 16.4 MB (25.79%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,22 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.replace("//","/")
+
+        dirs = path.split("/")
+        dirs = list(filter(lambda x: x!="", dirs))
+
+        st = []
+
+        for _dir in dirs:
+            if _dir == ".":
+                continue
+            elif st and _dir == "..":
+                st.pop()
+            else:
+                st.append(_dir)
+            
+
+        st = list(filter(lambda x: x!="" and x!='.' and x!='..', st))
+        print(st)
+        return '/'+'/'.join(st)
+            
