b629e74d1c2686a9057f6a502a30e7d8738d9829
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-08-27 12:41:12 -0400

    Time: 1243 ms (47.23%), Space: 41.6 MB (11.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,18 +1,20 @@
 from math import inf
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        #recurrence:
-        # dp[i] := min number of coins needed to make up amount i
-        # dp[i] = min_coin(dp[i-coin]+1) 
-        if amount == 0:
+        if not amount:
             return 0
-        dp = [float('inf') for _ in range(amount+1)]
-        dp[0] = 0
-        for i in range(amount+1):
-            for c in coins:
-                if i-c >= 0:
-                    dp[i] = min(dp[i-c]+1, dp[i])
-        return -1 if dp[amount]==float('inf') else dp[amount]
-
+        
+        memo = {}
+        def topDown(coins, amount):
+            if amount < 0:
+                return float('inf')
+            if amount == 0:
+                return 0
+            if amount in memo:
+                return memo[amount]
+            memo[amount] = min([topDown(coins, amount-c)+1 for c in coins])
+            return memo[amount]
+        ans = topDown(coins, amount) 
+        return -1 if ans == float('inf') else ans
 
         
