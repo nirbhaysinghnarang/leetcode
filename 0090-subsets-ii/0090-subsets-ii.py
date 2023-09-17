44dce6b991a9c7ce44c00f18ac3dca45cdaf3dd2
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-09-17 17:18:22 -0400

    Time: 54 ms (12.98%), Space: 16.3 MB (97.45%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        power_set = []
+        def dfs(index, path):
+            if index >= len(nums):
+                if not path in power_set:
+                    power_set.append(path.copy())
+                return
+            path.append(nums[index])
+            dfs(index+1, path)
+            path.pop()
+            dfs(index+1, path)
+        dfs(0, [])
+        return power_set
+                
